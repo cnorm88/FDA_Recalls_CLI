acb884e1f4f877073ee41f480063480f65044570
Crystal Norman <cnorm88@gmail.com>
2020-10-26 21:26:24 +0000

    update

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,11 @@
 class Api
-  # def self.fetch_drinks
-  #   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-  #   url = URI(url)
-  #   response = Net::HTTP.get(url)
-  #   hash = JSON.parse(response)
-  # end
+  def self.get_poster
+    url = "https://www.loc.gov/collections/yanker-posters?fo=json"
+    url = URI(url)
+    response = Net::HTTP.get(url)
+    hash = JSON.parse(response)
+  end
+
+binding.pry
 
 end
