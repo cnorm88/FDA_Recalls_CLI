e7d199307b38e55fd971d3c96d71e01edfe381f2
Crystal Norman <cnorm88@gmail.com>
2020-10-26 21:13:15 +0000

    updated classes

---
+++ lib/api.rb
@@ -0,0 +1,9 @@
+class Api
+  # def self.fetch_drinks
+  #   url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+  #   url = URI(url)
+  #   response = Net::HTTP.get(url)
+  #   hash = JSON.parse(response)
+  # end
+
+end
---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+def start
+  puts "Welcome"
+  Api.fetch_drinks
+  # self.menu
+end
