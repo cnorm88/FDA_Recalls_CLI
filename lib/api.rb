36e69b8bb65eea6c2de2b8a2927b8432a471a0c6
Crystal Norman <cnorm88@gmail.com>
2020-10-26 22:21:02 +0000

    updates

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,10 @@
 class Api
 
   def self.get_poster
-    url = "https://www.loc.gov/collections/yanker-posters?fo=json"
+    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
     url = URI(url)
     response = Net::HTTP.get(url)
     hash = JSON.parse(response)
-    binding.pry
   end
 
 
