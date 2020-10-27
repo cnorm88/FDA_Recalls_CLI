ad52f84b60e35a1c1f85da90c6c789b7b50f06b1
Crystal Norman <cnorm88@gmail.com>
2020-10-27 18:01:38 +0000

    cli methods update

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,20 @@
 class Cli
 
-def start
-  puts "Welcome"
-  Api.get_poster
-  # self.menu
+  def start
+    puts "Please enter your name:"
+    # name = gets.strip.capitalize
+    # puts "Welcome to the Yanker Poster collection search #{name}"
+    Api.get_poster
+    # self.menu
+  end
+
+  def display_list_of_posters
+  #access all the posters
+  #print each one out
+  Poster.all.each.with_index(1) do |poster, index|
+    puts "#{index}. #{poster.name}"
+  end
 end
+
 
 end
