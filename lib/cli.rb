7556c9906921455adb09b0f86c685ad59573d088
Crystal Norman <cnorm88@gmail.com>
2020-10-30 02:16:44 +0000

    formatted code

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,161 +2,96 @@ class Cli
 
   def start
     puts "Please enter your name:"
-    # name = gets.strip.capitalize
-    # puts "Welcome to the Yanker Poster collection search #{name}"
+    name = gets.strip.capitalize
+    puts "Welcome to the FDA recall information application #{name}"
     Api.get_report
     self.menu
   end
 
   def menu
-    #give user option to see list of drinks
+    sleep(1)
+    puts "\n"
     puts "Would you like to see a list of recalls made by the FDA in 2020?"
+    sleep(1)
+    puts "\n"
     puts "Type 'C' to continue or any other key to exit"
     user_input = gets.strip.downcase
-    #if the user types C
+
     if user_input == "C" || user_input == "c"
       puts "Here's a list!"
-      #give an indexed list of recalls
+
       list_of_recalls
       users_selection
+    else
       sleep(1)
       puts "\n"
-
-      # menu
-    else
       puts "Maybe next time"
     end
-
   end
 
   def list_of_recalls
-  #access all the posters
-  #print each one out
-  Reports.all.each.with_index(1) do |report, index|
-    report = report.name
-    puts "#{index}. #{report}"
-  end
-  end
-
-  #     puts "What would you like to know about this poster? Here are your options:
-  #     Title, Description, Link to image, Location"
-  #     choice = gets.chomp.capitalize
-  #     case choice
-  #  when "Title"
-  #      puts
-  #  end
-
-    #would you like to know which state the recall was located?
-    #the city, date, reason, product description and how many items were recalled?
-
-    #all the recalls made in a specific state or specific time frame
-    def users_selection
-      puts "Enter the number of the report you'd like to know more about"
-        index = gets.strip.to_i - 1
-        #index valid? #btwn 0 & 6
-      until index.between?(0, Reports.all.length - 1)
-    # keep asking for user input #???an EXIT
-      puts "Sorry invalid input. Choose a valid number"
-      index = gets.strip.to_i - 1
-      end
-
-      instance = Reports.all[index]
-      recall_details(instance)
+    Reports.all.each.with_index(1) do |report, index|
+      report = report.name
+      puts "#{index}. #{report}"
     end
+  end
 
-    # def exit_or_continue
-    #   puts "\n"
-    #    puts "If you would like to find out more information type Yes or a list of states type List or type any button to exit"
-    #
-    #       choice = gets.strip.capitalize
-    #
-    #      case choice
-    #
-    #      when "Yes"
-    #        recall_details(self)
-    #      when "List"
-    #         puts "#{report.name}"
-    #      else
-    #         puts "thank you"
-    #      end
-    #     # end
-    # end
+def users_selection
+  puts "Enter the number of the report you'd like to know more about"
+    index = gets.strip.to_i - 1
+  until index.between?(0, Reports.all.length - 1)
+    puts "Sorry invalid input. Choose a valid number"
+    index = gets.strip.to_i - 1
+  end
+  instance = Reports.all[index]
+  recall_details(instance)
+end
 
-    def recall_details(report)
-      puts "\n"
-      # report = report.name
-      puts "What would you like to know about this #{report.name} recall? Here are your options:
-      Location, Description, Date, Quantity"
-#for other options type exit
-      choice = gets.strip.capitalize
-
-     case choice
-
-  when "Location"
-       puts "State: #{report.state} City: #{report.city}"
-       recall_details(report)
-
-when "Description"
-      puts report.description
-      puts report.recall_reason
-      recall_details(report)
-  when "Date"
-    puts report.date
-    recall_details(report)
-
-  when "Quantity"
-      puts "\n"
-      puts report.quantity
-
+def recall_details(report)
+  puts "\n"
+  puts "What would you like to know about this #{report.name} recall? Here are your options: Location, Description, Date, Quantity"
+    choice = gets.strip.capitalize
+    case choice
+      when "Location"
+          puts "State: #{report.state} City: #{report.city}"
+            recall_details(report)
+      when "Description"
+        puts report.description
+        puts report.recall_reason
+          recall_details(report)
+      when "Date"
+        puts report.date
+        recall_details(report)
+      when "Quantity"
+        puts "\n"
+        puts report.quantity
+        recall_details(report)
      else
        exit_or_continue
      end
-     end
+    end
 
-    #  puts something to exit or "Sorry try again (method to try again) or type Options to acquire more information"
-   # calls recall_details(report) -> to try again
-   # ask would you like to try again ^ else "no" = exit
-
-   # when "Option"
-    #  puts "\n"
-    #  puts "Would you like to ? if Yes enter Y to continue and any button to exit"
-    #  user_input = gets.strip.downcase
-    #  if user_input == "Y" || user_input == "y"
-    #    puts "Lets continue"
-    #    #or a new method
-    #when "exit"
-
-    #  else
-    #    puts "thanks" //the exit
-
-  #the new method
-      #all the recalls made in a specific state or specific time frame
 def exit_or_continue
   puts "\n"
-   puts "If you would like to find out more information type Yes or a list of states type List or type any button to exit"
-
-      choice = gets.strip.capitalize
-
-     case choice
-
-     when "List"
-       find_by_state
-     else
+  puts "If you would like to find out more information type Yes or a list of states type List or type any button to exit"
+    choice = gets.strip.capitalize
+    case choice
+      when "List"
+        find_by_state
+      else
         puts "thank you"
-     end
-    # end
+    end
 end
 
 def find_by_state
   puts "\n"
   puts "Which state would you like to find?"
     state = gets.strip.upcase
-
-    Reports.all.each do |report|
+  Reports.all.each do |report|
     report = report.state
     puts "#{report.size} cases in #{state}" if report.strip.include? "#{state}"
 
-    end
+  end
     sleep (3)
     puts "\n"
     puts "No results found or try again"
